692ed5c0a6db66e682e70762a330387509d92054
tariel sapar <tariel.fifa2015@gmail.com>
2021-04-06 19:48:57 +0600

    Starting point (commit: 0)
    Created empty project

---
+++ src/main/java/com/attractor/peklo/PekloApplication.java
@@ -0,0 +1,13 @@
+package com.attractor.peklo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PekloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PekloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attractor/peklo/PekloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attractor.peklo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PekloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
